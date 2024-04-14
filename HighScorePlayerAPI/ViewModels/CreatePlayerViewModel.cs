8fc2d63de39cb6749bc11a2396000b715dc8bb53
Nicollas Diniz <nicollasfdiniz@gmail.com>
2024-04-14 19:55:15 -0300

    ADD --Controllers GET,POST AND DELETE

---
+++ HighScorePlayerAPI/ViewModels/CreatePlayerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HighScorePlayerAPI.ViewModels;
+
+public class CreatePlayerViewModel
+{
+    [Required]
+    public string Player { get; set; }
+    [Required]
+    public double Score { get; set; }
+}
